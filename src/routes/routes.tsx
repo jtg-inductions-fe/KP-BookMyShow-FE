d8258ed52529e38328019598d6475710d3d30969
kushal raj pareek <kushal.pareek@joshtechnologygroup.com>
2025-12-06 13:52:38 +0530

    [KP_A3_02] - Main layout.

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,11 +1,12 @@
 import { createBrowserRouter, RouteObject } from 'react-router-dom';
 
 import { APP_ROUTES } from '@constants';
+import { MainLayout } from '@layout';
 
 const routes: RouteObject[] = [
     {
         path: APP_ROUTES.HOME,
-        element: <>Bookmyshow</>,
+        element: <MainLayout />,
     },
 ];
 
